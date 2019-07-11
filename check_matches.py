70b6751b8993ce8f282a1e41dbe4fe3ac9968a2a
Crystal <czhu2020@spyponders.com>
2019-07-11 16:26:13 -0400

    added check_matches

---
+++ check_matches.py
@@ -0,0 +1,19 @@
+def check_matches(audio_data):
+    """
+    Checks if audio data from a song matches a song in the database, and if so, which song it is
+
+    Parameters
+    ----------
+    audio_data: np.array([])
+        An array of audio samples from recorded audio or audio file.
+    
+    Returns
+    -------
+    String
+        Either displays the name of the song and artist or says the the song isn't recognized.
+
+    Dependencies
+    ------------
+    ~~~
+
+    """
