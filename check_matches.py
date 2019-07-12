14ef40b1e6ebdac9abee2d2f56d93815ea7f573d
Crystal <czhu2020@spyponders.com>
2019-07-12 11:14:51 -0400

    finished function, ready to test and debug

--- check_matches.py
+++ check_matches.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import Counter
 import digital_samples_to_peaks2 as spk
 import fingerprint as fp
 
@@ -16,8 +17,8 @@ def check_matches(audio_data, database):
     
     Returns
     -------
-    String
-        Either displays the name of the song and artist or says the the song isn't recognized.
+    int()
+        The song id of the recognized song; -1 if no song recognized
 
     Dependencies
     ------------
@@ -28,24 +29,61 @@ def check_matches(audio_data, database):
 
     #data_to_time = dict(zip(audio_data, times)); probably don't need this,
     # but map data to time after peaks and keys are calculated
-    req = 10 #Determine through experimentation
-    no_match = "Song not recognized." #maybe -1?
+    req = 100 #Determine through experimentation
+    no_match = -1 #maybe -1?
 
-    kvpairs = fp.get_fingerprint(spk.sample_to_peaks(spk.sample_to_spectrogram(audio_data)), 15)
+    kvpairs = dict(fp.get_fingerprint(spk.sample_to_peaks(spk.sample_to_spectrogram(audio_data)), 15))
     #audio_data, times --> spectrogram --> array of peaks: peak = (t, f) --> list of peaks in the song
+    
+    #COUNT NUMBER OF MATCHED PEAKS FOR EACH SONG IN THE DATABASE, RECORD OFFSETS
+
+    match_cnt = Counter()
+
+    for k, v in kvpairs:
+        if k in database:
+            match_cnt.update((id, song_t - v[1]) for (id, song_t) in database[k])
+
+    #ELIMINATE SONGS WITH INSUFFICIENT MATCHES
+    for match in match_cnt:
+        if match_cnt[match] < req:
+            del match_cnt[match]
+
+    if len(match_cnt) == 0:
+        return no_match
+            
+    #RETURN THE SONG ID WITH THE MOST MATCHES
+    return match_cnt.most_common()
+
+    #OLD CODE--WILL PROBABLY NOT NEED (IGNORE)
+
+        '''
+        sample_time = np.array(v)
+        song_time = np.array(database[k])
+        sample_time[:,1] = song_time[:,1] - sample_time[:,1] #maybe will have to reverse, but this works with array broadcasting
+        match_cnt.update(sample_time)
+        '''
+
+        '''
+        for pair in v:
+            if pair[0] not in match_cnt:
+                match_cnt[pair[0]] = 1
+            else:
+                match_cnt[pair[0]] += 1
+        '''
+
+    '''
     times = list() #Times at which peaks were found in given song
     posmatches = list() #Values (lists of (song1, time), (song2, time)...) for possible matches
-
-    #FIND MATCHING PEAKS
+    '''
+    
+    '''
     for p, t in kvpairs:
         if p in database:
             times.append(t) #time at which matched peak was found (in given song) is appended to times
             posmatches.append(database[p]) #actual corresponding time and song values from database are recorded
-    
-    #RETURN NO MATCH IF INSUFFICIENT MATCHES
-    if len(posmatches) < req: #need to do this for every song?
-        return no_match
+    '''
 
+    '''
     #FIND OFFSETS
     t_matches = np.array(zip(times, posmatches)) #now includes column 0: given song's time values, column 1: time values from the database
     time_diffs = []
@@ -54,4 +92,4 @@ def check_matches(audio_data, database):
         #maybe make second term cleaner with numpy array later
         time_diffs.append(diff)
     
-    pass
+    '''
