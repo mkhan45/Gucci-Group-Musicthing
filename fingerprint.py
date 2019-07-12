1b78c8554d7ee8c12bff05709860e09865899c72
yehf-bxsci <yehf@bxscience.edu>
2019-07-12 02:31:20 -0400

    Finished fingerprinting functionality

--- fingerprint.py
+++ fingerprint.py
@@ -1,20 +1,61 @@
-def peak_to_fingerprint(peaks):
-    """
-    Converts the peaks to their respective key values
+def peaks_to_fingerprint(peaks, fanout, song_id=None):
+    """Converts the peaks to keys (and values) that represent the song.
+
+    Specifying a song_id will cause this function to output a dictionary.
+    Otherwise, the function will output a list of keys.
 
     Parameters
     ----------
-    peaks: np.ndarray.shape(N, 2)
-        A 2D array of `N` peaks, where np.axis=1 represents (frequency, time).
+    peaks : List[Tuple[int, int]]
+        A list of peaks, where each peak is given by Tuple[<freq>, <time>].
+    fanout : int
+        An int representing how many other peaks to relate, i.e. how many keys
+        to generate per peak.
+    song_id : int
+        The unique int that identifies the corresponding song to store in song_dict
 
     Returns
     -------
-    np.ndarray.shape(N)
-        $(f_i, f_n, t_n - T_i), n \in \N, [0, \text{fanout})$
-        The array of keys corresponding to each peak.
+    keys: List[Tuple[int, int, int]]
+        A list of keys, where each key is given by
+        List[Tuple[<freq_i>, <freq_n>, <dt>]]
+    song_dict : dict
+        The dictionary representing the song, where keys are given by
+        Tuple[<freq_i>, <freq_n>, <dt>], and values are given by
+        Tuple[`song_id`, <time>]
 
-    Notes: something about fanout values
+    Notes
+    -----
+    The keys are not returned in chronological order, and are formatted as follows:
+    .. math::  (f_i, f_n, t_n - t_i), \text{where} \, \{n \, | \, n \in \mathbb{N}, \, n \in [0, \mathtt{fanout}) \}
     """
 
+    if song_id is None:
+        keys = []
+        for i in range(1, fanout+1):
+            for j in range(len(peaks) - i):
+                keys.append(__get_key(peaks[j], peaks[j+i]))
+        return keys
+    else:
+        song_dict = {}
+        for i in range(1, fanout+1):
+            for j in range(len(peaks) - i):
+                song_dict[__get_key(peaks[j], peaks[j+i])] = (song_id, peaks[j][1])
+        return song_dict
 
-def keys_to_dictionary():
+
+def __get_key(peak1, peak2):
+    """Generates a key from two peaks.
+
+    Parameters
+    ----------
+    peak1, peak2 : List[Tuple[int, int]]
+        The two peaks to generate a key from.
+
+    Returns
+    -------
+    key : Tuple[int, int, int]
+        The key to be used in song_dict.
+    """
+
+    return (peak1[0], peak2[0], peak2[1] - peak1[1])
